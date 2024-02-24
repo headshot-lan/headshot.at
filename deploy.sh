351c46f362465c80fe8a9091fc1881448a2a4630
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2024-02-24 23:25:33 +0100

    chore: deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set +e
+
+# deploy klms
+# ./deploy.sh
+
+BASE_DIR=`dirname $0`
+
+# echo "Build prod assets"
+npm run build
+
+echo "Push data"
+rsync -avzh --exclude-from=".deployignore" --delete * -e "ssh -p 822" headshot_ftp@neu.headshot.at:/neu.headshot.at/
+
+echo "Rsynced data, clearing cache"
+ssh -p 822 headshot_ftp@neu.headshot.at "rm -rf /neu.headshot.at/var/cache/*"
+
